5116a3e49f50bc7c56efe3809b5dc39a5187ac47
madhu <email2madhu.poranki@gmail.com>
2018-07-21 18:13:58 +0530

    fixing first issue

--- Javalearnupdate/src/com/java/learn/Test.java
+++ Javalearnupdate/src/com/java/learn/Test.java
@@ -4,5 +4,6 @@ public class Test {
 public static void main(String[] args) {
 	System.out.println("UPDATE JAVA WITH GIT");
 	System.out.println("pushing changes from command prompt");
+	System.out.println("Creating saparate branch and using that");
 }
 }
