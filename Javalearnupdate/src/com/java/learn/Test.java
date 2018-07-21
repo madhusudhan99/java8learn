6450427ee1e239df867b93e85f67fdcadf8a7179
madhu <email2madhu.poranki@gmail.com>
2018-07-21 07:16:54 +0530

    commiting from command

--- Javalearnupdate/src/com/java/learn/Test.java
+++ Javalearnupdate/src/com/java/learn/Test.java
@@ -3,5 +3,6 @@ package com.java.learn;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("UPDATE JAVA WITH GIT");
+	System.out.println("pushing changes from command prompt");
 }
 }
