2e46dcf898fd7fc08522fc9414c8d0299eb5ce80
madhu <email2madhu.poranki@gmail.com>
2018-07-21 18:26:52 +0530

    adding second branch

--- Javalearnupdate/src/com/java/learn/Test.java
+++ Javalearnupdate/src/com/java/learn/Test.java
@@ -4,6 +4,6 @@ public class Test {
 public static void main(String[] args) {
 	System.out.println("UPDATE JAVA WITH GIT");
 	System.out.println("pushing changes from command prompt");
-	System.out.println("Creating saparate branch and using that");
+	System.out.println("Creating saparate branch and using that And adding code");
 }
 }
