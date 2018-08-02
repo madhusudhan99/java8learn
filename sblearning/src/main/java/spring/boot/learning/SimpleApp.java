f0d3eae987f4f772b05ce2dc42fae77db1720f07
madhu <email2madhu.poranki@gmail.com>
2018-08-02 07:39:01 +0530

    printing messages

--- sblearning/src/main/java/spring/boot/learning/SimpleApp.java
+++ sblearning/src/main/java/spring/boot/learning/SimpleApp.java
@@ -2,8 +2,10 @@ package spring.boot.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"spring.boot.learning","root.sam.web"})
 public class SimpleApp {
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleApp.class, args);
